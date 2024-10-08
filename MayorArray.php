5b1f7a98ba0296dce9834dd6a250ca642a9441fe
Julio Soto <156647271+Julio24pro@users.noreply.github.com>
2024-10-08 10:08:40 +0200

    Add files via upload

---
+++ MayorArray.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mayor valor de un array</title>
+</head>
+<body>
+
+<?php
+        // Declaración de un array con 7 valores
+        $numeros = [34, 67, 23, 89, 12, 90, 55];
+
+        // Encontrar el mayor valor usando la función max()
+        $mayor = max($numeros);
+
+        // Mostrar el mayor valor
+        echo "<h3>$mayor</h3>";
+?>
+
+</body>
+</html>
